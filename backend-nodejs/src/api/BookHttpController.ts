7c89fa3c162ce919bccf8741f167f8bbdea1a968
thilanka <thilanka.geshan@gmail.com>
2023-07-29 13:07:57 +0530

    Expose following Web Services

--- backend-nodejs/src/api/BookHttpController.ts
+++ backend-nodejs/src/api/BookHttpController.ts
@@ -2,4 +2,13 @@ import express from 'express';
 
 export const router = express.Router();
 
+router.delete('/:isbn', async (req, res) => {
+
+    res.send("<h1>Delete a book</h1>")
+
+});
+
+router.patch('/:isbn', async (req, res) => {
+    res.send("<h1>Update the book</h1>")
+});
 
