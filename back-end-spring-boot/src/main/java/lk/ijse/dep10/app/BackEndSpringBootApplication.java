f1162d03d39f487577b89227a9216a0ba967e801
thilanka <thilanka.geshan@gmail.com>
2023-07-30 16:20:35 +0530

    Finalize Spring Boot

--- back-end-spring-boot/src/main/java/lk/ijse/dep10/app/BackEndSpringBootApplication.java
+++ back-end-spring-boot/src/main/java/lk/ijse/dep10/app/BackEndSpringBootApplication.java
@@ -1,10 +1,17 @@
 package lk.ijse.dep10.app;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackEndSpringBootApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndSpringBootApplication.class, args);
--- back-end-spring-boot/src/main/java/lk/ijse/dep10/app/api/BookHttpController.java
+++ back-end-spring-boot/src/main/java/lk/ijse/dep10/app/api/BookHttpController.java
@@ -1,26 +1,32 @@
 package lk.ijse.dep10.app.api;
 
+import lk.ijse.dep10.app.business.BookBO;
+import lk.ijse.dep10.app.dto.BookDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1/books")
 public class BookHttpController {
 
+    private final BookBO bookBO;
+
+    public BookHttpController(BookBO bookBO) {
+        this.bookBO = bookBO;
+    }
 
     @GetMapping
-    public String getAllBooks(){
-        return "<h1>Get all books</h2>";
+    public List<BookDTO> getAllBooks() throws Exception {
+        return bookBO.getAllBooks();
     }
 
     @PostMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public String saveBook(){
-        return "<h1>Save Book</h1>";
+    public void saveBook(@RequestBody @Valid BookDTO bookDTO) throws Exception {
+        bookBO.saveBook(bookDTO);
     }
 }
